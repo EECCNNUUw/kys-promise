a33f6369c63a22315ffcbf3e2930e363de1fc532
sty <scarsty@gmail.com>
2025-07-16 19:05:08 +0800

    编译脚本

--- kys-promise-androidstudio/complie_so_apk.bat
+++ kys-promise-androidstudio/complie_so_apk.bat
@@ -1,11 +1,15 @@
+
 set path_project=D:\kys-all\promise\kys-promise\
 set path_ct=D:\kys-all\codetyphon\
 set project=kys_promise.lpr
 set output=libkys_promise.so
 
-
 %path_ct%\fpc\fpc64\bin\x86_64-win64\fpc.exe -Tandroid -Paarch64 -MDelphi -Scghi -CX -Os2 -Xs -XX -l -vewnhibq -Fi%path_project%tmp\android -Fl%path_project%lib -Fl%path_ct%\binLibraries\android-5.0.x-api21-aarch64 -Fl%path_ct%lib\arm64-v8a -Fu%path_project%lib -Fu%path_ct%\typhon\lcl\units\aarch64-android\customdrawn -Fu%path_ct%\typhon\lcl\units\aarch64-android -Fu%path_ct%\typhon\components\BaseUtils\lib\aarch64-android -FU%path_project%android -FE. -o%output% -dLCL -dadLCL -dLCLcustomdrawn %path_project%%project%
 
-copy %output% .\lib\arm64-v8a  
+copy %output% .\app\lib\arm64-v8a 
+
+call gradlew.bat assembleRelease 
+
+copy .\app\build\outputs\apk\release\*.apk . /y
 
 pause
